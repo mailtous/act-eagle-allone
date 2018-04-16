51dbbcb005d2fa602bcfd51e7ceaa65ee7acbb85
mailtous <mailtous@163.com>
2018-04-16 16:20:27 +0800

    fix:logger

--- src/main/java/com/artlongs/framework/controller/BaseController.java
+++ src/main/java/com/artlongs/framework/controller/BaseController.java
@@ -3,6 +3,8 @@ package com.artlongs.framework.controller;
 import act.app.ActionContext;
 import act.controller.Controller;
 import act.event.EventBus;
+import org.osgl.logging.L;
+import org.osgl.logging.Logger;
 
 import javax.inject.Inject;
 
@@ -11,6 +13,8 @@ import javax.inject.Inject;
  * Created by leeton on 9/25/17.
  */
 public class BaseController extends Controller.Util{
+
+    protected Logger logger = L.get(this.getClass());
 
     @Inject
     protected ActionContext ctx;
